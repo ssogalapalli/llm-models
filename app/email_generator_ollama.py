e4d0fe1f9e67be38020bcf98a36c6d0a73f2b928
ssogalapalli <kamballisiva@gmail.com>
2025-08-03 21:40:14 +0200

    email generator with ollama

--- app/email_generator_ollama.py
+++ app/email_generator_ollama.py
@@ -3,7 +3,7 @@ from connection.connect_ollama import connect_ollama
 ollama_connect_openai = connect_ollama()
 
 HEADERS = {"Content-Type": "application/json"}
-MODEL = "llama3.2"
+MODEL = " llama3.2"
 messages =[
     {"role": "user", "content": "Write an email to request for new resources in the team"}
 ]
